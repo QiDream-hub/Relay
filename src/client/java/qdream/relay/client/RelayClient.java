9d56b22c52e2e55d957adbb9623932cc6afec92e
祁梦迟月 <14922885+qi-meng-chi-yue@user.noreply.gitee.com>
2026-05-29 01:22:28 +0800

    初始化

---
+++ src/client/java/qdream/relay/client/RelayClient.java
@@ -0,0 +1,10 @@
+package qdream.relay.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RelayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
